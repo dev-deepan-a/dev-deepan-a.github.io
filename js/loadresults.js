59b847a0d42323753dec53c831ee3dd273f1d4f5
Deepan A <deepan@houseofbluebeans.com>
2019-11-29 13:10:28 +0530

    seekapp-web

---
+++ js/loadresults.js
@@ -0,0 +1,104 @@
+//var result_json = "{\"vanity\":[{\"bounding_boxes\":[\"0.45073718\",\"0.39470387\",\"0.91194683\",\"0.74000204\"],\"product_tags\":[\"36\",\"gray\",\"transitional\",\"vanity\"],\"similar_products\":[{\"img_url\":\"https:\/\/st.hzcdn.com\/simgs\/5cc11fdd0c6576da_4-2191\/home-design.jpg\",\"img_ext\":\"jpg\",\"product_url\":\"https:\/\/www.houzz.com\/products\/allentown-36-single-bathroom-vanity-gray-prvw-vr~138421819\"},{\"img_url\":\"https:\/\/www.bathvanityexperts.com\/media\/catalog\/product\/cache\/1\/image\/9df78eab33525d08d6e5fb8d27136e95\/5\/2\/527-v36-sl-2c3.jpg\",\"img_ext\":\"jpg\",\"product_url\":\"https:\/\/www.bathvanityexperts.com\/james-martin-palisades-527-v36-sl-36-inch-silver-gray-transitional-single-bathroom-vanity.html\"},{\"img_url\":\"https:\/\/i.pinimg.com\/originals\/6a\/8b\/3d\/6a8b3d8821714fe6ca4afd7293dcb11e.jpg\",\"img_ext\":\"jpg\",\"product_url\":\"https:\/\/www.pinterest.com\/pin\/412220172122705871\/\"}]}],\"toiletpaperholder\":[{\"bounding_boxes\":[\"0.566914\",\"0.13476309\",\"0.638491\",\"0.21306387\"],\"product_tags\":[\"1 piece\",\"toilet\"],\"similar_products\":[{\"img_url\":\"https:\/\/images.homedepot-static.com\/productImages\/889345f7-5c08-4ab3-aa1a-72afeb61697e\/svn\/white-glacier-bay-one-piece-toilets-n2420-31_1000.jpg\",\"img_ext\":\"jpg\",\"product_url\":\"https:\/\/www.homedepot.com\/p\/Glacier-Bay-1-Piece-1-1-GPF-1-6-GPF-High-Efficiency-Dual-Flush-Elongated-All-in-One-Toilet-in-White-N2420\/203076918\"},{\"img_url\":\"https:\/\/images.homedepot-static.com\/productImages\/f8cfc0db-173a-425f-ab24-cde13d8de3ff\/svn\/cotton-white-toto-one-piece-toilets-ms853113s-01-64_1000.jpg\",\"img_ext\":\"jpg\",\"product_url\":\"https:\/\/www.homedepot.com\/p\/TOTO-UltraMax-1-Piece-1-6-GPF-Single-Flush-Round-Toilet-in-Cotton-White-ms853113s-01\/206794386\"},{\"img_url\":\"https:\/\/mobileimages.lowes.com\/product\/converted\/739268\/739268117600.jpg?size=xl\",\"img_ext\":\"jpg\",\"product_url\":\"https:\/\/www.lowes.com\/pd\/TOTO-Eco-Ultramax-Cotton-White-WaterSense-Elongated-Standard-Height-1-Piece-Toilet-12-in-Rough-In-Size\/1000094419\"}]}],\"walltile\":[{\"bounding_boxes\":[\"0.25879127\",\"0.7096631\",\"0.3982743\",\"0.80369586\"],\"product_tags\":[],\"similar_products\":[]}],\"toilet\":[{\"bounding_boxes\":[\"0.54506844\",\"0.20998177\",\"0.92918545\",\"0.3635609\"],\"product_tags\":[\"1 piece\",\"toilet\"],\"similar_products\":[]}],\"faucet\":[{\"bounding_boxes\":[\"0.42374337\",\"0.52243036\",\"0.48603708\",\"0.61482674\"],\"product_tags\":[\"widespread\",\"vibrant brushed bronze\",\"faucet\"],\"similar_products\":[]}],\"floor\":[{\"bounding_boxes\":[\"0.914105\",\"0.138255\",\"0.99503917\",\"0.21092269\"],\"product_tags\":[],\"similar_products\":[]}],\"towelbar\":[{\"bounding_boxes\":[\"0.2204072\",\"0.17912996\",\"0.2933622\",\"0.40023065\"],\"product_tags\":[\"polished chrome\",\"towel bar\"],\"similar_products\":[{\"img_url\":\"https:\/\/images.homedepot-static.com\/productImages\/2a6ccbc0-3acd-407a-ad23-0297ad78d006\/svn\/polished-chrome-kohler-towel-bars-k-14451-cp-64_1000.jpg\",\"img_ext\":\"jpg\",\"product_url\":\"https:\/\/www.homedepot.com\/p\/KOHLER-Stillness-24-in-Towel-Bar-in-Polished-Chrome-K-14451-CP\/100065292\"},{\"img_url\":\"https:\/\/images.homedepot-static.com\/productImages\/2dc12409-12a6-4f2b-a1f3-b25112d5416e\/svn\/polished-chrome-kohler-towel-bars-k-10550-cp-64_1000.jpg\",\"img_ext\":\"jpg\",\"product_url\":\"https:\/\/www.homedepot.com\/p\/KOHLER-Devonshire-18-in-Towel-Bar-in-Polished-Chrome-K-10550-CP\/202388389\"},{\"img_url\":\"https:\/\/images.homedepot-static.com\/productImages\/cdeafe5e-f585-4a27-aed9-b8397299af9d\/svn\/chrome-delta-towel-bars-132886-64_1000.jpg\",\"img_ext\":\"jpg\",\"product_url\":\"https:\/\/www.homedepot.com\/p\/Delta-Silverton-24-in-Towel-Bar-in-Polished-Chrome-132886\/202051982\"}]}],\"vanity_light\":[{\"bounding_boxes\":[\"0.07577153\",\"0.39127484\",\"0.22850889\",\"0.45760843\"],\"product_tags\":[\"24\",\"gray\",\"traditional\",\"vanity\"],\"similar_products\":[{\"img_url\":\"https:\/\/images-na.ssl-images-amazon.com\/images\/I\/512T6ArszeL._SX466_.jpg\",\"img_ext\":\"jpg\",\"product_url\":\"https:\/\/www.amazon.com\/Fresca-Manchester-Traditional-Bathroom-Vanity\/dp\/B074FHV31V\"},{\"img_url\":\"https:\/\/images-na.ssl-images-amazon.com\/images\/I\/4175SDHY8pL._SY355_.jpg\",\"img_ext\":\"jpg\",\"product_url\":\"https:\/\/www.amazon.com\/Fresca-FVN2302GR-CMB-Hartford-Traditional-Bathroom\/dp\/B01M16YAK9\"},{\"img_url\":\"https:\/\/images.homedepot-static.com\/productImages\/f8d17f73-8dff-42be-bc2c-1cecb3dc713f\/svn\/fresca-vanities-with-tops-fvnhd0102gr-cmb-64_1000.jpg\",\"img_ext\":\"jpg\",\"product_url\":\"http:\/\/www.homedepot.com\/p\/Fresca-Hudson-24-in-W-Traditional-Bathroom-Vanity-in-Gray-with-Ceramic-Vanity-Top-in-White-with-White-Basin-FVNHD0102GR-CMB\/300859452?MERCH=REC-_-rv_gm_pip_rr-_-202025491-_-300859452-_-N\"}]}]}"
+
+//var json_data = JSON.parse(result_json)
+//var current_key = Object.keys(json_data)[0];
+//var current_product_name = json_data[current_key];
+//var current_product = current_product_name[0];
+//var similar_products = current_product["similar_products"];
+//console.log(similar_products);
+
+function load_results(result_json){
+    
+    var ir_results_container = document.getElementById('ir-results-container');
+    
+    var child = ir_results_container.lastElementChild;  
+    while (child) { 
+        ir_results_container.removeChild(child); 
+        child = ir_results_container.lastElementChild; 
+    } 
+    
+    if(Object.keys(json_data).length == 0)
+        alert("could not detect anything");
+    
+    var json_data = JSON.parse(result_json);
+    for(var index=0; index<Object.keys(json_data).length; index++){
+        var current_key = Object.keys(json_data)[index];
+        var current_product_name = json_data[current_key];
+
+        for(var i=0; i < current_product_name.length; i++){
+            var current_product = current_product_name[0];
+            var similar_products = current_product["similar_products"];
+            for(var j=0; j<similar_products.length; j++){
+                console.log("" + similar_products[j]["img_url"]);
+                
+                var result = document.createElement("div");
+                result.setAttribute("class", "result");
+
+                var img = document.createElement("img");
+                img.setAttribute("class", "result-img");
+                img.src = similar_products[j]["img_url"];
+
+                result.appendChild(img);
+
+                var a_tag = document.createElement("a");
+                a_tag.setAttribute("class", "img-meta-data");
+                a_tag.setAttribute("href", similar_products[j]["product_url"]);
+
+                var name_div = document.createElement("div");
+                name_div.innerHTML = similar_products[j]["product_name"];
+                name_div.style.width = "100%";
+                name_div.style.height = "25px";
+                name_div.style.overflow = "hidden";
+
+                var website_name_div = document.createElement("div");
+                website_name_div.innerHTML = similar_products[j]["website"];
+                website_name_div.style.width = "100%";
+                website_name_div.style.height = "25px";
+                website_name_div.style.overflow = "hidden";
+
+                a_tag.appendChild(name_div);
+                a_tag.appendChild(website_name_div);
+
+                result.appendChild(a_tag);
+
+                ir_results_container.appendChild(result);
+            }
+        }
+    }
+    
+    
+//    var ir_results_container = document.getElementById('ir-results-container');
+//    for(var i=0; i < 50; i++){
+//        var result = document.createElement("div");
+//        result.setAttribute("class", "result");
+//        
+//        var img = document.createElement("img");
+//        img.setAttribute("class", "result-img");
+//        img.src = "https://st.hzcdn.com/simgs/e1216b950c62cdca_4-5071/home-design.jpg";
+//        
+//        result.appendChild(img);
+//        
+//        var a_tag = document.createElement("a");
+//        a_tag.setAttribute("class", "img-meta-data");
+//        a_tag.setAttribute("href", "https://www.google.com/search?q=blue+vanity&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjq1Ib8rIzmAhVCH7cAHV0QDl8Q_AUoAnoECAsQBA&biw=1920&bih=937");
+//        
+//        var name_div = document.createElement("div");
+//        name_div.innerHTML = "Vibrant brushed nickel towel bar";
+//        name_div.style.width = "100%";
+//        name_div.style.height = "25px";
+//        name_div.style.overflow = "hidden";
+//        
+//        var website_name_div = document.createElement("div");
+//        website_name_div.innerHTML = "https:\\lowes.com";
+//        website_name_div.style.width = "100%";
+//        website_name_div.style.height = "25px";
+//        website_name_div.style.overflow = "hidden";
+//        
+//        a_tag.appendChild(name_div);
+//        a_tag.appendChild(website_name_div);
+//        
+//        result.appendChild(a_tag);
+//        
+//        ir_results_container.appendChild(result);
+//    }
+}
